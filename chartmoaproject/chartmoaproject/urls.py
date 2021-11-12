a4fa39b09b8c2612999514c85d3c6ce4cb9fb14e
hellozo0 <ahju0011@gmail.com>
2021-11-12 20:45:17 +0900

    현주 - 기본 세팅

--- chartmoaproject/chartmoaproject/urls.py
+++ chartmoaproject/chartmoaproject/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from chartmoa.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name="main"),
+    path('chart/', chart, name="chart"),
+    path('developers/', developers, name="developers"),
 ]
